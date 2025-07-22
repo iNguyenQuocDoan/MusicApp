9870586365d15a04bd380d498f0c719bd33ecfa8
iNguyenQuocDoan <quocdoanfb@gmail.com>
2025-07-23 00:02:36 +0700

    test

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,8 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    config.resolve.alias['@/components'] = path.resolve(__dirname, 'src/app/components');
-    return config;
-  },
-
-  reactStrictMode: false
+  reactStrictMode: false,
 };
 
 export default nextConfig;
