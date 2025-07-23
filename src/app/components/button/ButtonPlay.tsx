35b3da5dd352b4602215d0156b9ebd5a35e5a4a2
iNguyenQuocDoan <quocdoanfb@gmail.com>
2025-07-23 15:59:25 +0700

    tua

--- src/app/components/button/ButtonPlay.tsx
+++ src/app/components/button/ButtonPlay.tsx
@@ -1,56 +1,77 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
-import { FaPlay } from "react-icons/fa";
+import { FaPlay } from "react-icons/fa6";
 
 export default function ButtonPlay(props: any) {
-  const { id, image, title, singer, listen, link, audio } = props;
+  const { id, image, title, singer, audio, className = "" } = props;
+
   const handlePlay = () => {
     const elementPlayAudio = document.querySelector(".play-audio");
-    // phát nhạc
+
     if (elementPlayAudio) {
+      // Chèn song-id
+      elementPlayAudio.setAttribute("song-id", id);
+
+      // Phát nhạc
       const elementAudio: any = elementPlayAudio.querySelector(".inner-audio");
       const elementSource: any =
         elementPlayAudio.querySelector(".inner-source");
       elementSource.src = audio;
       elementAudio.load();
       elementAudio.play();
-    }
 
-    // hiện thị play audio
-    if (elementPlayAudio.classList.contains("hidden")) {
-      elementPlayAudio.classList.remove("hidden");
-    }
+      // Hiển thị khối Play
+      if (elementPlayAudio.classList.contains("hidden")) {
+        elementPlayAudio.classList.remove("hidden");
+      }
 
-    // Hiển thị ảnh
-    const elementImage: any = elementPlayAudio.querySelector(".inner-image");
-    if (elementImage) {
+      // Hiển thị ảnh
+      const elementImage: any = elementPlayAudio.querySelector(".inner-image");
       elementImage.src = image;
       elementImage.alt = title;
-    }
-    // Hiển thị tiêu đề
-    const elementTitle: any = elementPlayAudio.querySelector(".inner-title");
-    if (elementTitle) {
+
+      // Hiển thị tiêu đề
+      const elementTitle: any = elementPlayAudio.querySelector(".inner-title");
       elementTitle.innerHTML = title;
-    }
 
-    // Hiển thị tên ca sĩ
-    const elementSinger: any = elementPlayAudio.querySelector(".inner-singer");
-    if (elementSinger) {
+      // Hiển thị ca sĩ
+      const elementSinger: any =
+        elementPlayAudio.querySelector(".inner-singer");
       elementSinger.innerHTML = singer;
-    }
 
-    // Thêm class active vào button play
-    const elementButtonPlay: any = document.querySelector(".inner-button-play");
-    if (elementButtonPlay) {
+      // Thêm class play vào inner-button-play
+      const elementButtonPlay: any =
+        elementPlayAudio.querySelector(".inner-button-play");
       elementButtonPlay.classList.add("play");
+
+      // Lấy ra tổng thời gian
+      const elementPlayTimeTotal: any = elementPlayAudio.querySelector(
+        ".inner-play-time .inner-total"
+      );
+      const elementPlayTimeCurrent: any = elementPlayAudio.querySelector(
+        ".inner-play-time .inner-current"
+      );
+
+      elementAudio.onloadedmetadata = () => {
+        const totalTime = elementAudio.duration;
+        elementPlayTimeTotal.max = totalTime;
+
+        // Lấy ra thời gian hiện tại
+        elementAudio.ontimeupdate = () => {
+          const currentTime = elementAudio.currentTime;
+          elementPlayTimeTotal.value = currentTime;
+          const percent = (currentTime * 100) / totalTime;
+          elementPlayTimeCurrent.style.width = `${percent}%`;
+        };
+      };
     }
   };
+
   return (
-    <button
-      onClick={handlePlay}
-      className="w-[34px] h-[34px] rounded-full border border-white inline-flex items-center justify-center text-[15px] mr-[10px]"
-    >
-      <FaPlay />
-    </button>
+    <>
+      <button onClick={handlePlay} className={className}>
+        <FaPlay />
+      </button>
+    </>
   );
 }
--- src/app/components/play/PlayTime.tsx
+++ src/app/components/play/PlayTime.tsx
@@ -1,14 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
 export default function PlayTime() {
+  // tua
+  const handleChange = (event: any) => {
+    const time = parseFloat(event.target.value);
+    const elementAudio: any = document.querySelector(
+      ".play-audio .inner-audio"
+    );
+    elementAudio.currentTime = time;
+  };
   return (
     <>
-      <div className="mt-[11px] relative">
-        <div className="h-[4px] w-0 bg-[#00ADEF] rounded-[50px] absolute left-0 top-[13px]"></div>
+      <div className="mt-[11px] relative inner-play-time">
+        <div className="h-[4px] w-0 bg-[#00ADEF] rounded-[50px] absolute left-0 top-[13px] inner-current"></div>
         <input
           type="range"
           min={0}
-          max={100}
+          max={0}
           defaultValue={0}
-          className="w-full h-[4px] bg-[#FFFFFF0A] cursor-pointer range-sm"
+          className="w-full h-[4px] bg-[#FFFFFF0A] cursor-pointer range-sm inner-total"
+          onChange={(event) => handleChange(event)}
         />
       </div>
     </>
